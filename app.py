a7fd1342edd52303569a131b6deec81925b57f17
IsmatQasim <ismat6812@gmail.com>
2024-05-05 18:01:39 +0500

    final changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from g4f.client import Client
+import speech_recognition as sr
 
 app = Flask(__name__)
 client = Client()
@@ -12,6 +13,26 @@ def chat_with_gpt(input_message):
     )
     return response.choices[0].message.content
 
+def convert_speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Please speak something...")
+        recognizer.adjust_for_ambient_noise(source)  
+        audio_data = recognizer.listen(source)  
+
+    try:
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio_data)
+        print(text)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, I couldn't understand what you said.")
+        return ""
+    except sr.RequestError as e:
+        print("Sorry, there was an error processing your request. {0}".format(e))
+        return ""
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -21,6 +42,11 @@ def chat():
     user_input = request.form['user_input']
     bot_response = chat_with_gpt(user_input)
     return jsonify({'bot_response': bot_response})
+
+@app.route('/speech-to-text', methods=['POST'])
+def speech_to_text():
+    text = convert_speech_to_text()
+    return jsonify({'text': text})
 
 if __name__ == "__main__":
     app.run(debug=True)
